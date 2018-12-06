80fe457dd20a5069ecc9b1dfaa3af047b57c4d24
Argyro-Sioziou <arguro_s@outlook.com>
2018-12-06 14:41:39 +0200

    Adding routers and minor changes

--- client/abg/src/app/app.component.ts
+++ client/abg/src/app/app.component.ts
@@ -3,12 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  show:boolean = false;
 
-  toggleCollapse() {
-    this.show = !this.show
-  }
+export class AppComponent {
+    
 }
--- client/abg/src/app/app.module.ts
+++ client/abg/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { GamesComponent } from './games/games.component';
 import { RatingsComponent } from './ratings/ratings.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GamesComponent,
-    RatingsComponent
+    RatingsComponent,
+    DashboardComponent,
+    ReviewsComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
