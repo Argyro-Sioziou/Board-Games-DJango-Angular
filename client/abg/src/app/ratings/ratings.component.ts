f3273804c705897ea16a1cdbf9af4fedef040552
Argyro-Sioziou <arguro_s@outlook.com>
2018-12-06 14:46:59 +0200

    Updating ratings component

--- client/abg/src/app/ratings/ratings.component.ts
+++ client/abg/src/app/ratings/ratings.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Game } from '../game';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-ratings',
@@ -9,21 +8,12 @@ import { Game } from '../game';
 
 export class RatingsComponent implements OnInit {
 
-  game: Game = {
-    id: 1,
-    name: "Wizard",
-    rules: "Rules",
-    price: 10,
-    rate: 5,
-    min_age: 10,
-    max_age: 100,
-    min_num_of_players: 1,
-    max_num_of_players: 100,
-    creator: "KAISSA",
-    tags: ["Μπλόφα", "Χαρτιά"],
-  }
+  @Input()
+  readonly: String;
 
-  readonly = false;
+  @Input()
+  rate: number;
+
   hovered = 0;
 
   constructor() {
